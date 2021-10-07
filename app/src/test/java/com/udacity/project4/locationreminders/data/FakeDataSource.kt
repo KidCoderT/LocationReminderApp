0ca489795e82d1591dedfdb00ab95c33b9ab51f4
Tejas <Tejas75O25@gmail.com>
2021-10-07 07:59:01 +0530

    Added testing to reminderListView but error in testing

--- app/src/test/java/com/udacity/project4/locationreminders/data/FakeDataSource.kt
+++ app/src/test/java/com/udacity/project4/locationreminders/data/FakeDataSource.kt
@@ -2,18 +2,18 @@ package com.udacity.project4.locationreminders.data
 
 import com.udacity.project4.locationreminders.data.dto.ReminderDTO
 import com.udacity.project4.locationreminders.data.dto.Result
+import com.udacity.project4.locationreminders.reminderslist.ReminderDataItem
 
 //Use FakeDataSource that acts as a test double to the LocalDataSource
-class FakeDataSource : ReminderDataSource {
-
-//    TODO: Create a fake data source to act as a double to the real data source
+class FakeDataSource(var reminders: MutableList<ReminderDTO>? = mutableListOf()) : ReminderDataSource {
 
     override suspend fun getReminders(): Result<List<ReminderDTO>> {
-        TODO("Return the reminders")
+        reminders?.let { return Result.Success(ArrayList(it)) }
+        return Result.Error("Reminders not found")
     }
 
     override suspend fun saveReminder(reminder: ReminderDTO) {
-        TODO("save the reminder")
+        reminders?.add(reminder)
     }
 
     override suspend fun getReminder(id: String): Result<ReminderDTO> {
@@ -21,7 +21,7 @@ class FakeDataSource : ReminderDataSource {
     }
 
     override suspend fun deleteAllReminders() {
-        TODO("delete all the reminders")
+        reminders?.clear()
     }
 
 
--- app/src/test/java/com/udacity/project4/locationreminders/reminderslist/RemindersListViewModelTest.kt
+++ app/src/test/java/com/udacity/project4/locationreminders/reminderslist/RemindersListViewModelTest.kt
@@ -1,7 +1,17 @@
 package com.udacity.project4.locationreminders.reminderslist
 
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.udacity.project4.locationreminders.data.FakeDataSource
+import com.udacity.project4.locationreminders.data.dto.ReminderDTO
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import org.hamcrest.MatcherAssert.assertThat
+import org.hamcrest.core.Is.`is`
+import org.junit.Assert
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
@@ -9,5 +19,44 @@ import org.junit.runner.RunWith
 class RemindersListViewModelTest {
 
     //TODO: provide testing to the RemindersListViewModel and its live data objects
+
+    private lateinit var fakeDataSource: FakeDataSource
+    private lateinit var remindersViewModel: RemindersListViewModel
+
+    @get:Rule
+    var instantTaskRule = InstantTaskExecutorRule()
+
+    @Before
+    fun setupTestViewModel() {
+        fakeDataSource = FakeDataSource()
+        remindersViewModel = RemindersListViewModel( ApplicationProvider.getApplicationContext(), fakeDataSource)
+    }
+
+    @Test
+    fun getShowNoData_noReminder_returnsTrue() {
+        // When
+        remindersViewModel.loadReminders()
+
+        // Then
+        assertThat(remindersViewModel.showNoData.value, `is`(true))
+    }
+
+    @Test
+    suspend fun getShowNoData_hasReminder_returnsFalse() {
+        // Given
+        fakeDataSource.saveReminder(ReminderDTO(
+            "title",
+            "Description",
+            "location",
+            55.55,
+            45.55
+        ))
+
+        // When
+        remindersViewModel.loadReminders()
+
+        // Then
+        assertThat(remindersViewModel.showNoData.value, `is`(false))
+    }
 
 }

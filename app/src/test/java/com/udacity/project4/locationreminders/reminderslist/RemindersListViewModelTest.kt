c22638107b3cb27f10d307503f3f870934ebbd1e
Tejas <Tejas75O25@gmail.com>
2021-10-08 15:24:41 +0530

    Finished ReminderListViewModel Testing

--- app/src/test/java/com/udacity/project4/locationreminders/reminderslist/RemindersListViewModelTest.kt
+++ app/src/test/java/com/udacity/project4/locationreminders/reminderslist/RemindersListViewModelTest.kt
@@ -1,5 +1,6 @@
 package com.udacity.project4.locationreminders.reminderslist
 
+import android.os.Build
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
@@ -8,6 +9,7 @@ import com.udacity.project4.locationreminders.data.FakeDataSource
 import com.udacity.project4.locationreminders.data.dto.ReminderDTO
 import com.udacity.project4.locationreminders.rule.MainCoroutineRule
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.runBlockingTest
 import org.hamcrest.MatcherAssert.assertThat
 import org.hamcrest.core.Is.`is`
 import org.junit.Assert
@@ -15,9 +17,12 @@ import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
+import org.koin.core.context.stopKoin
+import org.robolectric.annotation.Config
 
 @RunWith(AndroidJUnit4::class)
 @ExperimentalCoroutinesApi
+@Config(sdk = [Build.VERSION_CODES.O])
 class RemindersListViewModelTest {
 
     //TODO: provide testing to the RemindersListViewModel and its live data objects
@@ -33,17 +38,52 @@ class RemindersListViewModelTest {
 
     @Before
     fun setupTestViewModel() {
+        stopKoin()
         fakeDataSource = FakeDataSource()
         remindersViewModel = RemindersListViewModel( ApplicationProvider.getApplicationContext(), fakeDataSource)
     }
 
     @Test
-    fun getShowNoData_noReminder_returnsTrue() {
+    fun checkShowNoData_returnsTrue_onNoReminders() {
         // When
         remindersViewModel.loadReminders()
 
         // Then
         assertThat(remindersViewModel.showNoData.value, `is`(true))
+    }
+
+    @Test
+    fun checkShowNoData_returnsFalse_onHasReminders() = runBlockingTest {
+        // Given
+        fakeDataSource.saveReminder(ReminderDTO(
+            "title",
+            "Description",
+            "location",
+            55.55,
+            45.55
+        ))
+
+        // When
+        remindersViewModel.loadReminders()
+
+        // Then
+        assertThat(remindersViewModel.showNoData.value, `is`(false))
+    }
+
+    @Test
+    fun checkShowLoadingValue_returnsTrueFalse_onloadReminderRunningAndCompleted() {
+        // When
+        mainCoroutineRule.pauseDispatcher() // Pause coroutines
+        remindersViewModel.loadReminders()
+
+        // Then
+        assertThat(remindersViewModel.showLoading.value, `is`(true))
+
+        // When
+        mainCoroutineRule.resumeDispatcher()
+
+        // Then
+        assertThat(remindersViewModel.showLoading.value, `is`(false))
     }
 
 }
